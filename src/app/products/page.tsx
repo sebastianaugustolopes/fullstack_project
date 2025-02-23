40791518de086ed7a3c2165804b612321aae6b0e
sebastianaugustolopes <sebastianaugustolopes@gmail.com>
2025-02-23 00:21:44 -0300

    feat: add shadcd initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button"
+
+const ProductPage = () => {
+    return <div className="p-5 border-2 border-red-800 rounded-xl">
+        <h1 className="text-red-500">Produto</h1>
+        <Button>FW 7.0</Button>
+    </div>;
+}
+ 
+export default ProductPage;
